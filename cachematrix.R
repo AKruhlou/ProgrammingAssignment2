4611ebcc6ab3b48396ef688d315bb75f0d3d30d4
AKruhlou <akruhlou@gmail.com>
2016-03-26 17:28:31 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Finding the inverse matrix
+## work with the cache
 
-## Write a short comment describing this function
+## Return the list of functions for cache: set(x), get(), set_inv(x), get_inv()
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        m_inv <- NULL
+        
+        # cached the value of the matrix and clear its inverse value 
+        set <- function(x) {
+                m <<- x
+                m_inv <<- NULL
+        }
+        # get the value of the matrix "m" from cache 
+        get <- function() m
+        
+        # cached the value of the inverse matrix
+        set_inverse <- function(x) m_inv <<- x
+        # get the value of the inverse matrix "m_inv" from cache
+        get_inverse <- function() m_inv
+    
+        list(set = set, get = get, set_inv = set_inverse, get_inv = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'm'
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        
+        # try to retrieve the inverse matrix from the cache
+        m_inv <- m$get_inv()
+        
+        if(!is.null(m_inv)) {
+            message("getting cached inverse matrix")
+            return(m_inv)
+        }
+        # find the inverse matrix and cached it
+        m_inv <- solve(m$get(), ...)
+        m$set_inv(m_inv)
+        
+        # control: retrieve the inverse value from the cache
+        m$get_inv()
 }
